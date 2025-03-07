893d8f5d2be351ed8885e47735f1b317f9ff13c3
James <jbpitts@gmail.com>
2025-03-07 16:20:26 -0600

    fix: mistake in chuckArray

--- src/gen/actions/chunkArray.ts
+++ src/gen/actions/chunkArray.ts
@@ -1,8 +1,7 @@
 export function chunkArray<T>(array: T[], chunkCount: number): T[][] {
-  const chunkSize = Math.ceil(array.length / chunkCount);
   const chunks: T[][] = [];
-  for (let i = 0; i < array.length; i += chunkSize) {
-    chunks.push(array.slice(i, i + chunkSize));
+  for (let i = 0; i < array.length; i += chunkCount) {
+    chunks.push(array.slice(i, i + chunkCount));
   }
   return chunks;
 }
